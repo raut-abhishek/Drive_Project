15a042782308dfbc6f44167b1eefb4d8f454fb76
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-09-09 23:58:33 +0530

    user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const {body, validationResult} = require('express-validator');
+const userModel = require('../models/user.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 
 
@@ -12,7 +15,7 @@ router.post('/register',
     body('email').trim().isEmail().isLength({min:10}),
     body('password').trim().isLength({min:5}),
     body('username').trim().isLength({min:3}),
-    (req, res)=>{
+    async (req, res)=>{
         const errors = validationResult(req);
         if(!errors.isEmpty()){
            return res.status(400).json({
@@ -20,8 +23,69 @@ router.post('/register',
             message: "Invalid data"
            })
         }
-        res.send(errors);
+        const {email, username, password} = req.body;
+
+        const hashPassword = await bcrypt.hash(password, 10)
+
+        const newUser = await userModel.create({
+            email,
+            username,
+            password: hashPassword
+        })
+        res.json(newUser);
 })
 
+
+// login section
+router.get('/login', (req, res)=>{
+    res.render('login');
+})
+router.post('/login',
+    body('username').trim().isLength({min:3}),
+    body('password').trim().isLength({min:5}),
+    async (req, res)=>{
+
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                error: errors.array(),
+                message: Data
+            })
+        }
+
+        const {username, password} = req.body;
+
+        const user = await userModel.findOne({
+            username : username
+        })
+        if(!user){
+            return res.status(400).json({
+                message: 'username or password is incorrect'
+            })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(400).json({
+                message: 'username or password is incorrect'
+            })
+        }
+
+
+        // Token creation
+        const token = jwt.sign({
+            userID: user._id,
+            emil : user.email,
+            username : user.username
+        },
+            process.env.JWT_SECRET 
+        )
+
+        res.cookie('token', token)
+        res.send('Logged in')
+ 
+    }
+ )
 
 module.exports = router;
