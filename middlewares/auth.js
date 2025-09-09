69a1b4b2a134f5d4862de89a8ad2b802efa03422
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-09-09 23:58:11 +0530

    authentication middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+
+
+function auth(req, res, next){
+    const token = req.cookies.token;
+
+    if(!token){
+        return res.status(401).json({
+            message: 'unauthorised'
+        })
+    }
+
+    try{
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = decoded;
+        return next();
+
+    } catch(err){
+
+        return res.status(401).json({
+            message: 'Unauthorised'
+        })
+    }
+}
+
+
+module.exports = auth;
