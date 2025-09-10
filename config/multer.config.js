a49efa1689a8e0529f06fa96e44ea1f854189c80
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-09-10 13:28:20 +0530

    deployment changes

--- config/multer.config.js
+++ config/multer.config.js
@@ -1,22 +1,22 @@
-const multer = require('multer');
-const firebaseStorage = require('multer-firebase-storage');
-const firebase = require('./firebase.config');
-const serviceAccount = require('../drive-d12c1-firebase-adminsdk-fbsvc-67b66aa146.json')
+const multer = require("multer");
+const firebaseStorage = require("multer-firebase-storage");
+const firebase = require("./firebase.config");
 
 const storage = firebaseStorage({
-    credential: firebase.credential.cert(serviceAccount),
-    bucketName: 'drive-d12c1.firebasestorage.app',
-    unique: true,
-    credentials: {
-        projectId: serviceAccount.project_id,
-        clientEmail: serviceAccount.client_email,
-        privateKey: serviceAccount.private_key,
-    }
+  credential: firebase.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
+  bucketName: "drive-d12c1.firebasestorage.app",
+  unique: true,
+  credentials: {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  },
 });
 
-
-const upload = multer({
-    storage: storage
-})
+const upload = multer({ storage });
 
 module.exports = upload;
