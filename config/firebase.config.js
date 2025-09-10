6e22091bc8ebf109d771fdee74e906c13b6d6d25
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-09-10 13:05:54 +0530

    deployment changes

--- config/firebase.config.js
+++ config/firebase.config.js
@@ -1,10 +1,18 @@
-const Firebase = require('firebase-admin');
-const serviceAccount = require('../drive-d12c1-firebase-adminsdk-fbsvc-9d07a30ad6.json');
+const Firebase = require("firebase-admin");
+
+if (!process.env.FIREBASE_PROJECT_ID ||
+    !process.env.FIREBASE_CLIENT_EMAIL ||
+    !process.env.FIREBASE_PRIVATE_KEY) {
+  throw new Error("Missing Firebase environment variables");
+}
 
 const firebase = Firebase.initializeApp({
-    credential: Firebase.credential.cert(serviceAccount),
-    storageBucket:'drive-d12c1.firebasestorage.app'
+  credential: Firebase.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
+  storageBucket: "drive-d12c1.firebasestorage.app",
 });
-
 
 module.exports = Firebase;
